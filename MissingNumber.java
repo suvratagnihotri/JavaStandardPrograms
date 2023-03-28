f04230e0ee21aa4d7753089247abb5597eab77aa
Suvrat Agnihotri <suvrat72000@gmail.com>
2023-03-28 07:51:21 +0530

    Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.

---
+++ MissingNumber.java
@@ -0,0 +1,25 @@
+package com.java.StandardPrograms;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+    public int missingNumberInArray(int [] array){
+        int missingNUmber = 1;
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+        for(int i = 0; i<array.length; i++){
+            if(i+1==array[i]){
+                System.out.println("Number found");
+            }
+            else{
+                return i+1;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int A[] = {6,1,2,8,3,4,7,10,5};
+        System.out.println(new MissingNumber().missingNumberInArray(A));
+    }
+}
