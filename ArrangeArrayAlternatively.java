ecfac316679e891edf9fad371677d66fc5c48c00
Suvrat Agnihotri <suvrat72000@gmail.com>
2023-04-02 10:47:07 +0530

    Given a sorted array of positive integers. Your task is to rearrange the array elements alternatively i.e first element should be max value, second should be min value, third should be second max, fourth should be second min and so on. Note: Modify the original array itself. Do it without using any extra space. You do not have to return anything.

---
+++ ArrangeArrayAlternatively.java
@@ -0,0 +1,30 @@
+package com.java.StandardPrograms;
+
+import java.util.Arrays;
+
+// Given a sorted array of positive integers. 
+// Your task is to rearrange the array elements
+//  alternatively i.e first element should be max value,
+//   second should be min value, third should be second max, 
+//   fourth should be second min and so on.
+
+public class ArrangeArrayAlternatively {
+    public int [] alternate(int array[]){
+        int temp = 0;
+        for(int i = 0; i<array.length; i++){
+            if(i%2==0){
+                temp = array[array.length-1];
+                for (int j = (array.length - 2); j >= i; j--) {   
+                    array[j+1] = array[j];
+                }
+                array[i] = temp;
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int [] array = {1,2,3,4,5,6};
+        System.out.println(Arrays.toString(new ArrangeArrayAlternatively().alternate(array)));
+    }
+}
