16b9894d3a5c790065c155672a54bbe7a4903cfe
Suvrat Agnihotri <suvrat72000@gmail.com>
2023-04-23 21:58:34 +0530

    Program to get the minimum number of characters required to form a palindrome string from a given string:

---
+++ Palindrome.java
@@ -0,0 +1,18 @@
+package com.java.StandardPrograms;
+
+public class Palindrome {
+    static int findMinInsertions(char str[], int l,int h){
+        if (l > h) return Integer.MAX_VALUE;
+        if (l == h) return 0;
+        if (l == h - 1) return (str[l] == str[h])? 0 : 1;
+ 
+        return (str[l] == str[h])?
+        findMinInsertions(str, l + 1, h - 1):
+        (Integer.min(findMinInsertions(str, l, h - 1),findMinInsertions(str, l + 1, h)) + 1);
+    }
+
+    public static void main(String[] args) {
+        String str= "geeks";
+        System.out.println(findMinInsertions(str.toCharArray(), 0, str.length()-1));
+    }
+}
