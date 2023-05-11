ae420f8b8b7eac4d5f356cfe1388b0a1d554e426
Suvrat Agnihotri <suvrat72000@gmail.com>
2023-05-11 22:56:04 +0530

    Program to create a queue using linked list

---
+++ Queue/Queue.java
@@ -0,0 +1,61 @@
+package com.java.StandardPrograms.Queue;
+
+import java.util.NoSuchElementException;
+
+public class Queue<T> {
+    private Node<T> front;
+    private Node<T> rear;
+    
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+        
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    
+    public Queue() {
+        this.front = null;
+        this.rear = null;
+    }
+    
+    public boolean isEmpty() {
+        return front == null;
+    }
+    
+    public void enqueue(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (isEmpty()) {
+            front = rear = newNode;
+        } else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+    }
+    
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        T data = front.data;
+        front = front.next;
+        if (front == null) {
+            rear = null;
+        }
+        return data;
+    }
+    
+    public T peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        return front.data;
+    }
+
+    public static void main(String[] args) {
+        //
+    }
+}
+
