60eab75a4e2fbaf9ee11506b04aae0ed51b17c9c
Suvrat Agnihotri <suvrat72000@gmail.com>
2023-05-14 19:03:17 +0530

    Program to create a stack using linked list

---
+++ Stack/Stack.java
@@ -0,0 +1,96 @@
+package com.java.StandardPrograms.Stack;
+
+
+public class Stack<T> {
+    Node <T> top;
+    int size;
+    public static class Node<T>{
+        private T data;
+        private Node<T> next;
+
+        public Node(T data){
+            this.data= data;
+            this.next = null;
+        }
+    }
+
+    public Stack(){
+        this.top = null;
+        this.size = 0;
+    }
+
+    public boolean isEmpty(){
+        return top == null;
+    }
+
+    public void push(T data){
+        Node<T> node = new Node<>(data);
+        if(isEmpty()){
+            this.top = node;
+        }
+        else{
+            node.next = top;
+            top = node;
+            size++;
+        }
+    }
+
+    public T pop(){
+        if(isEmpty()){
+            throw new RuntimeException("Stack is empty");
+        }
+        T data = top.data;
+        top = top.next;
+        size --;
+        return data;
+    }
+
+    public T peek(){
+        if(isEmpty()){
+            throw new RuntimeException("Stack is empty");
+        }
+        else{
+            return top.data;
+        }
+    }
+
+    public void printStack(){
+        if(isEmpty()){
+            throw new RuntimeException("Stack is empty.");
+        }
+        else{
+            Node<T> temp = top;
+            while(temp!=null){
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+
+        stack.push("prince");
+        stack.push("suvrat");
+        stack.push("agnihotri");
+        stack.push("papa");
+        stack.push("mummy");
+        stack.push("kushik");
+        stack.push("aman");
+        stack.push("ashish");
+        stack.push("ramdev");
+        stack.push("modi");
+
+        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println("_______________________________");
+        System.out.println(stack.peek());
+        System.out.println("=============================");
+        stack.printStack();
+
+
+
+    }
+}
