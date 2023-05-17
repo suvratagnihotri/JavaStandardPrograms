8d1b65716011a2c9aa86f6aea735a9e39003d250
Suvrat Agnihotri <suvrat72000@gmail.com>
2023-05-17 22:54:41 +0530

    program to check a string if it is balanced or not

---
+++ ParentisisChecker.java
@@ -0,0 +1,40 @@
+package com.java.StandardPrograms;
+
+public class ParentisisChecker {
+    public boolean isBalanced(String givenString){
+        int countSmallBracket = 0; int countBigBracket = 0;int countCurleyBracket = 0;
+        for(int i =0; i<givenString.length(); i++){
+            switch (givenString.charAt(i)){
+                case '(':
+                    countSmallBracket ++;
+                    break;
+                case ')':
+                    countSmallBracket --;
+                    break;
+                case '{':
+                    countCurleyBracket ++;
+                    break;
+                case '}':
+                    countCurleyBracket --;
+                    break;
+                case '[':
+                    countBigBracket ++;
+                    break;
+                case ']':
+                    countBigBracket --;
+                    break;
+            }
+
+        }
+        if(countSmallBracket==countCurleyBracket && countCurleyBracket==countBigBracket && countBigBracket==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ParentisisChecker().isBalanced("({({[]})})"));
+    }
+}
